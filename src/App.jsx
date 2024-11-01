3017f0c9e3763cbd5396870c5a21c906eeaa1b08
John B <johnbspamacc@outlook.com>
2024-10-31 23:53:13 -0700

    Initial project setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//other imports from components
+
+function App() {
+
+
+    return(
+        <Router>
+            
+        </Router>
+    );
+};
+
+export default App;
